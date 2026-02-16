581e2a76257e04f06ff978af885ee8865ba0f72c
Shravan Sheth <shravansheth@Mac.lan>
2026-02-15 16:13:12 -0800

    added more passes to remarks. still need more

--- scripts/run_opt_emit_ll.sh
+++ scripts/run_opt_emit_ll.sh
@@ -10,10 +10,31 @@ INPUT=$1
 OUTPUT=$2
 REMARKS=$3
 
+OUTDIR="$(dirname "$OUTPUT")"
+BASENAME="$(basename "$OUTPUT" .ll)"
+
+# ---- Run O2 pipeline ----
 opt "$INPUT" \
   -passes="default<O2>" \
   -S \
   -o "$OUTPUT" \
   -pass-remarks-output="$REMARKS" \
-  -pass-remarks=licm,gvn,dse \
-  -pass-remarks-analysis=licm,gvn,dse
+  -pass-remarks=licm,gvn,dse,cse,sccp,loops  \
+  -pass-remarks-analysis=licm,gvn,dse,cse,sccp,loops
+
+
+# ---- CFG generation----
+cd "$OUTDIR"
+
+opt -passes=dot-cfg -disable-output "$(basename "$OUTPUT")"
+
+for dotfile in *.dot .*.dot; do
+  [ -f "$dotfile" ] || continue
+
+  funcname="$(basename "$dotfile" .dot)"
+  funcname="${funcname#.}"   # strip leading dot
+
+  dot -Tpng "$dotfile" -o "${funcname}.O2.png"
+done
+
+echo "O2 CFG PNGs written"
